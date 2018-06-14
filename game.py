a06332c9cc275357845881e1aabd9c5ba1effc30
Rico Bachmann <rbbachmann@bluewin.ch>
2018-06-14 03:15:34 +0200

    initial game, no KI implemented, TODO: Add tested KI stuff as nice code

---
+++ game.py
@@ -0,0 +1,99 @@
+import numbers
+
+
+class GameModel:
+    fields = 0
+    allfields = 0
+    startbeans = 0
+    allbeans = []
+
+    currentPlayer = None
+    p1 = None
+    p2 = None
+
+    def __init__(self, beans, fields, p1, p2):
+        print("fields: " + str(fields))
+        self.fields = fields
+        self.allfields = (fields + 1) * 2
+        self.startbeans = beans
+        self.p1 = p1
+        self.p2 = p2
+        self.currentPlayer = self.p1
+
+        for x in range(0, self.allfields):
+            if x == (self.allfields / 2) - 1 or x == self.allfields - 1:
+                self.allbeans.append(0)
+            else:
+                self.allbeans.append(self.startbeans)
+
+    def do_move_p1(self, move):
+        print("seed the beans from " + str(move))
+        playbeans = self.allbeans[move]
+        print("beans " + str(playbeans))
+        self.allbeans[move] = 0
+        index = move + 1
+        while playbeans > 0:
+            if self.allfields - 1 == index:
+                index += 1
+            if self.allfields == index:
+                index = 0
+            self.allbeans[index] += 1
+            playbeans -= 1
+            if playbeans == 0 and self.allbeans[index] == 1 and (self.allfields / 2) - 1 != index:
+                self.allbeans[int(self.allfields / 2) - 1] += 1
+                self.allbeans[index] = 0
+                # add values from vis a vis
+                diff = self.fields - index
+                wonbeans = self.allbeans[self.fields + diff]
+                self.allbeans[self.fields + diff] = 0
+                self.allbeans[int(self.allfields / 2) - 1] += wonbeans
+            index += 1
+        self.currentPlayer = self.p2
+        if not isinstance(self.currentPlayer, numbers.Number):
+            print("run bot next movew")
+            self.p2.nextMove()
+
+    def do_move_p2(self, move):
+        print("seed the beans from " + str(move))
+        playbeans = self.allbeans[move]
+        print("beans " + str(playbeans))
+        self.allbeans[move] = 0
+        index = move + 1
+        while playbeans > 0:
+            if (self.allfields / 2) - 1 == index:
+                index += 1
+            if self.allfields == index:
+                index = 0
+            self.allbeans[index] += 1
+            playbeans -= 1
+            if playbeans == 0 and self.allbeans[index] == 1 and self.allfields - 1 != index:
+                self.allbeans[self.allfields - 1] += 1
+                self.allbeans[index] = 0
+                # add values from vis a vis
+                diff = self.allfields - 1 - index
+                print("diff "+ str(diff))
+                wonbeans = self.allbeans[-1 + diff]
+                self.allbeans[-1 + diff] = 0
+                self.allbeans[self.allfields - 1] += wonbeans
+            index += 1
+        self.currentPlayer = self.p1
+        if not isinstance(self.currentPlayer, numbers.Number):
+            print("run bot next movew")
+            self.p1.nextMove()
+
+    def do_move(self, player, move):
+        if self.currentPlayer == player:
+            if player == self.p1:
+                if 0 <= move < self.fields and self.allbeans[move] > 0:
+                    self.do_move_p1(move)
+                else:
+                    print("not allowed!!!!")
+            elif player == self.p2:
+                if self.allfields / 2 <= move < self.allfields - 1 and self.allbeans[move] > 0:
+                    self.do_move_p2(move)
+                else:
+                    print("not allowed!!!! ")
+            else:
+                print("unknown player")
+        else:
+            print("not your turn")
---
+++ mancala.py
@@ -0,0 +1,119 @@
+import numbers
+from tkinter import *
+from game import GameModel
+
+
+class MancalaView:
+    gamebeans = 4
+    gamefields = 6
+    gamewindow = None
+    gamep1 = None
+    gamep2 = None
+    ui_p1 = None
+    ui_p2 = None
+    gamerefreshdelay = 50
+    gamepadding = 20
+    gameipadding = 10
+    gamemodel = None
+
+    def __init__(self):
+        # create window for view
+        self.gamewindow = Tk()
+        self.gamewindow.title("MancalaSim")
+        self.gamewindow.minsize(600, 200)
+
+        # start refreshing view
+        self.gamewindow.after(self.gamerefreshdelay, self.update_view)
+
+    def on_p1click(self, idx):
+        # if real player do click function
+        if isinstance(self.gamep1, numbers.Number):
+            print("red p1 button move: " + str(idx))
+            self.gamemodel.do_move(self.gamep1, idx)
+
+    def on_p2click(self, idx):
+        # if real player do click function
+        if isinstance(self.gamep2, numbers.Number):
+            print("blue p2 button move: " + str(idx))
+            self.gamemodel.do_move(self.gamep2, idx)
+
+    def create_view(self, beans, fields):
+        self.gamebeans = beans
+        self.gamefields = fields
+
+        self.ui_p1 = []
+        cnt = 0
+        x = 0
+        for x in range(0, fields):
+            self.ui_p1.append(Button(self.gamewindow, text=str(beans), command=lambda idx=cnt: self.on_p1click(idx)))
+            self.ui_p1[x].configure(bg="orange red", fg="black")
+            self.ui_p1[x].grid(row=1, column=fields - x, pady=self.gamepadding,
+                               padx=self.gamepadding, ipady=self.gameipadding, ipadx=self.gameipadding)
+            cnt += 1
+        self.ui_p1.append(Label(self.gamewindow, text="0"))
+        self.ui_p1[fields].configure(bg="orange red", fg="black")
+        self.ui_p1[fields].grid(row=3, column=fields - 1 - x, pady=self.gamepadding,
+                                padx=self.gamepadding, ipady=self.gameipadding, ipadx=self.gameipadding)
+        self.ui_p1.append(Label(self.gamewindow, text="P1"))
+        self.ui_p1[fields + 1].configure(bg=self.gamewindow.cget('bg'), fg=self.gamewindow.cget('bg'))
+        self.ui_p1[fields + 1].grid(row=1, column=fields - 1 - x, pady=self.gamepadding,
+                                    padx=self.gamepadding, ipady=self.gameipadding, ipadx=self.gameipadding)
+
+        self.ui_p2 = []
+        cnt += 1
+        for x in range(0, fields):
+            self.ui_p2.append(Button(self.gamewindow, text=str(beans), command=lambda idx=cnt: self.on_p2click(idx)))
+            self.ui_p2[x].configure(bg="blue", fg="white")
+            self.ui_p2[x].grid(row=5, column=x + 1, pady=self.gamepadding,
+                               padx=self.gamepadding, ipady=self.gameipadding, ipadx=self.gameipadding)
+            cnt += 1
+        self.ui_p2.append(Label(self.gamewindow, text="0"))
+        self.ui_p2[fields].configure(bg="blue", fg="white")
+        self.ui_p2[fields].grid(row=3, column=fields + 1, pady=self.gamepadding,
+                                padx=self.gamepadding, ipady=self.gameipadding, ipadx=self.gameipadding)
+        self.ui_p2.append(Label(self.gamewindow, text="P2"))
+        self.ui_p2[fields + 1].configure(bg=self.gamewindow.cget('bg'), fg=self.gamewindow.cget('bg'))
+        self.ui_p2[fields + 1].grid(row=5, column=fields + 1, pady=self.gamepadding,
+                                padx=self.gamepadding, ipady=self.gameipadding, ipadx=self.gameipadding)
+
+    def create_game(self, p1, p2):
+        self.gamep1 = p1
+        self.gamep2 = p2
+
+        self.gamemodel = GameModel(self.gamebeans, self.gamefields, self.gamep1, self.gamep2)
+
+        self.gamewindow.mainloop()
+
+    def update_view(self):
+        cnt = 0
+        for x in self.ui_p1:
+            if cnt <= self.gamefields:
+                x.config(text=str(self.gamemodel.allbeans[cnt]))
+            else:
+                if self.gamemodel.currentPlayer == self.gamep1:
+                    x.configure(bg="black", fg="orange red")
+                else:
+                    x.configure(bg=self.gamewindow.cget('bg'), fg=self.gamewindow.cget('bg'))
+            cnt += 1
+
+        cnt = 0
+        for x in self.ui_p2:
+            if cnt <= self.gamefields:
+                x.config(text=str(self.gamemodel.allbeans[(self.gamefields + 1) + cnt]))
+            else:
+                if self.gamemodel.currentPlayer == self.gamep2:
+                    x.configure(bg="white", fg="blue")
+                else:
+                    x.configure(bg=self.gamewindow.cget('bg'), fg=self.gamewindow.cget('bg'))
+            cnt += 1
+
+        self.gamewindow.after(self.gamerefreshdelay, self.update_view)
+
+
+# create the game
+player1 = 1
+player2 = 2
+
+mancalagame = MancalaView()
+mancalagame.create_view(4, 6)
+mancalagame.create_game(player1, player2)
